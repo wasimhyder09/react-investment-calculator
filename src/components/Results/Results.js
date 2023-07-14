79e348499a4ed1106ac49cb0d0f7f55ddd3603e2
Wasim Hyder <wasim@creativefaze.com>
2023-07-14 22:16:49 +0500

    Displayed results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+
 const Results = (props) => {
-  console.log(props.data);
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
   return(
     <table className="result">
       <thead>
@@ -7,18 +15,21 @@ const Results = (props) => {
           <th>Year</th>
           <th>Total Savings</th>
           <th>Interest (Year)</th>
-          <th>Total Interest</th>
           <th>Invested Capital</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>YEAR NUMBER</td>
-          <td>TOTAL SAVINGS END OF YEAR</td>
-          <td>INTEREST GAINED IN YEAR</td>
-          <td>TOTAL INTEREST GAINED</td>
-          <td>TOTAL INVESTED CAPITAL</td>
-        </tr>
+        {Object.values(props.data).map((value, index) => {
+          return(
+          <tr key={index}>
+            <td>{value.year}</td>
+            <td>{formatter.format(value.savingsEndOfYear)}</td>
+            <td>{formatter.format(value.yearlyInterest)}</td>
+            <td>{formatter.format(value.yearlyContribution)}</td>
+          </tr>
+          )
+       })}
+        
       </tbody>
     </table>
   );
